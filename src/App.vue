67d2af1c3a23861b5fb4154fb2fb0921e757d424
周必川 <zhoubichuan@icloud.com>
2025-06-04 23:45:09 +0800

    更新模板

--- src/App.vue
+++ src/App.vue
@@ -1,30 +1,13 @@
-<script setup lang="ts">
-import { watchEffect, ref } from 'vue';
-import { RouterView, useRoute } from 'vue-router';
-import { ElConfigProvider } from 'element-plus';
-import basket from '@/components/basket/basket.vue';
-const route = useRoute();
-const isShowBasket = ref(false)
-watchEffect(() => {
-    console.log('Current route is', route.path);
-    if (route.path.includes('/sub-project/course-book') || route.path.includes('/sub-project/all-book/arrangement')) {
-        isShowBasket.value = true
-    } else {
-        isShowBasket.value = false
-    }
-});
-
-</script>
-
 <template>
     <div class="app">
-        <ElConfigProvider size="large" namespace="utb-ep">
+        <ElConfigProvider size="large" namespace="web-base">
             <RouterView />
         </ElConfigProvider>
     </div>
-    <basket v-if="isShowBasket" />
 </template>
-
+<script setup lang="ts">
+import { ElConfigProvider } from 'element-plus';
+</script>
 <style lang="scss">
 @import '@/styles/index.scss';
 
--- src/web-components/table-column/index.ts
+++
@@ -1,7 +0,0 @@
-import TableColumn from './src/table-column.vue';
-/* istanbul ignore next */
-TableColumn.install = function (Vue) {
-    Vue.component(TableColumn.name, TableColumn);
-};
-export default TableColumn;
-//
